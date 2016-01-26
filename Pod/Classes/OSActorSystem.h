5f85d9452f6005dc14362616652624b6f0e97f62
Natalia Grinevich <nata.grine@gmail.com>
2016-01-26 14:03:39 +0200

    Fix warnings when using 'class' c++ keyword

--- Pod/Classes/OSActorSystem.h
+++ Pod/Classes/OSActorSystem.h
@@ -16,7 +16,7 @@
 @property(nonatomic, readonly) OSServiceLocator *serviceLocator;
 @property (nonatomic, readonly) id<OSConfigs> configs;
 
-- (OSActorRef *)actorOfClass:(Class)class caller:(id)caller;
+- (OSActorRef *)actorOfClass:(Class)aClass caller:(id)caller;
 - (void)addActorProvider:(id<OSActorProvider>)actorProvider;
 
 @end
